198b5e590ac83b5f26559b2b5d909fcccd21cada
Felix Palta <fpalta90@gmail.com>
2015-06-13 19:42:30 +0300

    Add comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main()
 {
-    WeatherData w;
-    CurrentConditions cond(w);
+    WeatherData w; // The Observable Subject.
+    CurrentConditions cond(w); // One of the Observers subscribed to the Subject.
 
-    w.set_measurements(34, 50, 120);
+    w.set_measurements(34, 50, 120); // Changes in the Subject are pushed as notifications to the Observers.
     w.set_measurements(12, 34, 126);
     return 0;
 }
